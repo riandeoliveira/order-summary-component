7bca89d99b343588facf864c4420d7a27d709312
Rian Oliveira <riandiasdeoliveira2001@gmail.com>
2022-03-23 11:48:56 -0300

    📦 refactor: Update ```Card.ts```

--- src/styles/Card.ts
+++ src/styles/Card.ts
@@ -1,17 +1,20 @@
 import styled from "styled-components";
 
 export const CardContainer = styled.div`
-  background-color: var(--bg-card-color);
+  background-color: var(--white-color);
   border-radius: 20px;
+  box-shadow: 0px 30px 30px var(--portage-color);
   margin: 20px;
   max-width: 450px;
 `;
 
-export const HeaderArea = styled.header``;
+export const HeaderArea = styled.header`
+  text-align: center;
+`;
 
 export const MusicListener = styled.img`
-  border-top-left-radius: inherit;
-  border-top-right-radius: inherit;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
   max-width: 100%;
 `;
 
@@ -24,31 +27,41 @@ export const DescriptionContainer = styled.div`
 `;
 
 export const Title = styled.h1`
-  color: var(--title-color);
+  animation: fade-in 1s, slide-in 1s;
+  color: var(--cloud-burst-color);
   font-size: 28px;
   font-weight: 900;
   margin-bottom: 20px;
 `;
 
 export const Description = styled.p`
-  color: var(--text-color);
+  animation: fade-in 1s;
+  color: var(--bermuda-gray-color);
   font-size: 17px;
   line-height: 1.7em;
   margin-bottom: 20px;
-  padding: 0 12px;
 `;
 
 export const SubscriptionContainer = styled.div`
   align-items: center;
-  background-color: var(--bg-subs-color);
+  background-color: var(--zircon-color);
   border-radius: 10px;
   display: flex;
   justify-content: space-between;
   padding: 25px;
+
+  @media screen and (max-width: 420px) {
+    flex-direction: column;
+  }
 `;
 
 export const PlanContainer = styled.div`
+  align-items: center;
   display: flex;
+
+  @media screen and (max-width: 420px) {
+    margin-bottom: 20px;
+  }
 `;
 
 export const MusicalNote = styled.img``;
@@ -62,20 +75,22 @@ export const PriceContainer = styled.div`
 `;
 
 export const Plan = styled.span`
-  color: var(--plan-color);
+  color: var(--cloud-burst-color);
   font-weight: 900;
 `;
 
 export const Price = styled.span`
-  color: var(--price-color);
+  color: var(--bermuda-gray-color);
 `;
 
 export const Link = styled.a`
   font-size: 14px;
   font-weight: 700;
+  transition: all 0.2s linear;
 
   &:hover {
-    color: var(--change-color-hover);
+    color: var(--cornflower-blue-color);
+    text-decoration: none;
     transition: all 0.2s linear;
   }
 `;
@@ -88,25 +103,37 @@ export const OptionsContainer = styled.div`
 `;
 
 export const AcceptButton = styled.button`
-  background-color: var(--btn-accept-color);
-  border: none;
-  border-radius: 8px;
-  box-shadow: 1px 20px 15px var(--btn-accept-shadow-color);
-  color: var(--option-text-color);
+  animation: increase 1s;
+  background-color: var(--persian-blue-color);
+  border-radius: 12px;
+  box-shadow: 1px 20px 20px var(--portage-color);
+  color: var(--white-color);
   cursor: pointer;
-  font-family: var(--font);
   font-size: 15px;
   font-weight: 700;
-  padding: 14px;
+  padding: 16px;
+  transition: all 0.2s linear;
 
   &:hover {
-    background-color: var(--btn-accept-color-hover);
+    background-color: var(--cornflower-blue-color);
     transition: all 0.2s linear;
   }
 `;
 
+export const DeclineButtonContainer = styled.div`
+  margin-top: 30px;
+`;
+
 export const DeclineButton = styled.button`
-  color: var(--btn-cancel-color-hover);
+  background-color: transparent;
+  color: var(--bermuda-gray-color);
   cursor: pointer;
+  font-size: 15px;
+  font-weight: 900;
   transition: all 0.2s linear;
+
+  &:hover {
+    color: var(--cloud-burst-color);
+    transition: all 0.2s linear;
+  }
 `;
